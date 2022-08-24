87e7a89eeee78d0c122acb72c0202461c9f51ec7
Andres Barilin <v.barilin@itecriocuarto.org.ar>
2022-08-24 00:10:00 -0300

    Request to youtube

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,19 @@
-const API = 'https://rickandmortyapi.com/api/character'
+const API = 'https://youtube138.p.rapidapi.com/channel/videos/?id=UCUm3Zwa99TvX5tvybESNCiA&hl=en&gl=US'
 const content = null || document.getElementById('content')
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'abba6f849fmshf14b66c50ed68a0p1b9a09jsn6dab8a207d9e',
+		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+	}
+};
 
-const fetchData = async (urlAPI) => {
+
+const fetchData = async (urlAPI, options) => {
 
   try {
-    const response = await fetch(urlAPI)
+    const response = await fetch(urlAPI, options)
     const json = await response.json()
   
     return json
@@ -18,19 +26,22 @@ const fetchData = async (urlAPI) => {
 
 async function boardRicknMorty () {
 
-  const personajes = await fetchData(API)
+  const all = await fetchData(API, options)
+  
+  const videos = all.contents.filter(video => !video.video.title.includes('#SHORT'))
+  console.log(videos)
 
   try {
-    let view = `${personajes.results.map(personaje => `
+    let view = `${videos.map(video => `
     <div class="group relative">
       <div
         class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-        <img src=${personaje.image} alt=${personaje.name} class="w-full">
+        <img src=${video.video.thumbnails[0].url} alt=${video.video.title} class="w-full">
       </div>
       <div class="mt-4 flex justify-between">
-        <h3 class="text-sm text-gray-700">
+        <h3 class="text-sm text-gray-400">
           <span aria-hidden="true" class="absolute inset-0"></span>
-          ${personaje.name}
+          ${video.video.title}
         </h3>
       </div>
     </div>
